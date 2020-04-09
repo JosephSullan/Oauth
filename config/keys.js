8453ee1a3d93acaca026173052aa78784dfd9982
JosephSullan <58454989+JosephSullan@users.noreply.github.com>
2020-04-09 12:43:41 +0800

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+    google:{
+        clientID: '279628075478-nbhr6a4ki4od0559h4brsbjj0hrnecs0.apps.googleusercontent.com',
+        clientSecret: 'RHN6xrXsWr8wiaI6xIxMbV2x'
+    },
+    postgresdb:{
+        user: 'ldrviaqgsoltwk', 
+        host: 'ec2-54-221-214-183.compute-1.amazonaws.com',
+        database: 'd6tp11fvktrvd8', 
+        password: '5d62d0f5b69f8ce8a473a48d445f8c541f7ae61f9eb60b1c547af1b317a4114d', 
+        port:'5432',
+        ssl:true,
+    },session:{
+        cookieKey: 'mycookie'
+    }
+};
